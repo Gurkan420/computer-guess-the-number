c283feeb3bc8e96047ace4e43783ad124aa15e87
Gurkan420 <august.ljungdell@skola.botkyrka.se>
2020-11-09 15:57:59 +0100

    WIP

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const MIN_NUMBER = 0
 
 let guess
 
-document.getElementById('too-high-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too high?
+// 4. Avkommentera den sista raden i funktionen `start`. Det är ett exempel på **string interpolation**.
+
+
+document.getElementById('too-high-btn').addEventListener("click", function () {
+    
+setMessage(`Is it ${guess = guess -1}?`)
 
 })
+document.getElementById("is-correct-btn").addEventListener("click", function () {
+    setMessage("WOHOOOOOOOOOo")
+})
 
-document.getElementById('too-low-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too low?
+document.getElementById("too-low-btn").addEventListener("click", function () {
+    setMessage(`Is it ${guess++}?`)
     
 })
 
@@ -24,10 +31,13 @@ function start() {
 
     guess = 0
 
-    // setMessage(`Is it ${guess}?`)
+    setMessage(`Is it ${guess}?`)
 }
-
 document
     .getElementById('start-btn')
     .addEventListener('click', start)
+    
+    
+setMessage()
+
 
